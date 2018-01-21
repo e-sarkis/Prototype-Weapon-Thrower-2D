75cf102b5cb10a3ded5a6c8eb1c66b7025f8dd88
Elisha Sarkis <elisha.sarkis96@gmail.com>
2018-01-20 21:44:30 -0500

    Create and build out simple movement in PlayerMovement

---
+++ Assets/PlayerMover.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMover : MonoBehaviour 
+{
+	float moveSpeed = 1.0f;
+
+	private PlayerController _playerController;
+	private Rigidbody2D _rb2d;
+
+	void Awake()
+	{
+		_playerController 	= GetComponent<PlayerController>();
+		_rb2d 				= GetComponent<Rigidbody2D>();
+	}
+
+	void Update () 
+	{
+		if ( Mathf.Pow(_playerController.axisInputDirectionMovement.x, 2) > 0)
+		{
+			_rb2d.velocity = new Vector2(moveSpeed * _playerController.axisInputDirectionMovement.x, _rb2d.velocity.y);
+		} else
+		{
+			_rb2d.velocity = new Vector2(0, _rb2d.velocity.y);
+		}
+	}
+}
