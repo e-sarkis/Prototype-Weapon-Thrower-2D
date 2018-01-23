8dbd4a3c8c9075495b2012a501de59a5bfa453d2
Elisha <elisha.sarkis96@gmail.com>
2018-01-22 20:26:54 -0500

    Implement cheap collision ignore hack

--- Assets/_Scripts/PlayerHitboxContactDetector.cs
+++ Assets/_Scripts/PlayerHitboxContactDetector.cs
@@ -2,15 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerHitboxContactDetector : MonoBehaviour {
+public class PlayerHitboxContactDetector : MonoBehaviour 
+{
+	public List<GameObject> ignore;
 
-	// Use this for initialization
-	void Start () {
-		
+	private Collider2D _c2d;
+
+	void Start () 
+	{
+		_c2d = GetComponent<Collider2D>();
+
+		Collider2D otherC2d;
+		foreach (GameObject gObj in ignore)
+		{
+			otherC2d = gObj.GetComponent<Collider2D>();
+			if (otherC2d)
+			{
+				Physics2D.IgnoreCollision(_c2d, otherC2d);
+				Physics2D.IgnoreCollision(otherC2d, _c2d);
+			}
+		}
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
 }
